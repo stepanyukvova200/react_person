d00b4e769624ead48451c594502f2218e24c2fd1
Vova Stepanyuk <stepanyukvova200@gmail.com>
2024-04-17 03:47:48 +0300

    add solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// export const Person = ({ person }) => ();
+import '../../App.scss';
+
+export const Person = ({ name, age, sex, isMarried, PartnerName }) => {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age !== undefined
+        && <p className="Person__age">I am {age}</p>
+      }
+      <p className="Person__partner">
+        {!isMarried
+          && 'I am not married'
+        }
+        {isMarried
+          && sex === 'm'
+          && `${PartnerName} is my wife`
+        }
+        {isMarried
+          && sex === 'f'
+          && `${PartnerName} is my husband`
+        }
+      </p>
+    </section>
+  );
+};
