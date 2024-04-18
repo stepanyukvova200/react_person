7b8408ea3bbc7ac7bfa7dd974caff3a4a1c3b7a7
Vova Stepanyuk <stepanyukvova200@gmail.com>
2024-04-18 18:15:05 +0300

    add getMarriageStatus

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,21 @@
+const getMarriageStatus = (person) => {
+  if (!person.isMarried) {
+    return 'I am not married';
+  } else if (person.sex === 'm') {
+    return `${person.partnerName} is my wife`;
+  } else if (person.sex === 'f') {
+    return `${person.partnerName} is my husband`;
+  }
+};
+
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age !== undefined && (
-      <p className="Person__age">I am {person.age}</p>
-    )}
+    {person.age !== undefined
+      && <p className="Person__age">I am {person.age}</p>
+    }
     <p className="Person__partner">
-      {!person.isMarried && 'I am not married'}
-      {person.isMarried &&
-        person.sex === 'm' &&
-        `${person.partnerName} is my wife`}
-      {person.isMarried &&
-        person.sex === 'f' &&
-        `${person.partnerName} is my husband`}
+      {getMarriageStatus(person)}
     </p>
   </section>
 );
